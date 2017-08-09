4df71c4f3217bca61d794867da4a4e981d97ca8a
jack850628 <jack850628@gmail.com>
2017-08-10 02:09:18 +0800

    增加WebView的提示視窗

--- app/src/main/java/com/example/jack8/floatwindow/initWindow.java
+++ app/src/main/java/com/example/jack8/floatwindow/initWindow.java
@@ -1,14 +1,20 @@
 package com.example.jack8.floatwindow;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
 import android.webkit.WebBackForwardList;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * 初始化視窗內容
@@ -27,7 +33,7 @@ public class initWindow {
                 break;
         }
     }
-    public static void initWindow1(Context context,View v,final View winform,final WindowManager wm,final WindowManager.LayoutParams wmlp){
+    public static void initWindow1(final Context context, final View v, final View winform, final WindowManager wm, final WindowManager.LayoutParams wmlp){
         final EditText path=(EditText)v.findViewById(R.id.webpath);
         path.setText("https://www.google.com.tw/?gws_rd=ssl");
         Button go=(Button)v.findViewById(R.id.go);
@@ -39,6 +45,82 @@ public class initWindow {
             public boolean shouldOverrideUrlLoading(WebView view, String url){//當點擊WebView內的連結時處理，參考:https://dotblogs.com.tw/newmonkey48/2013/12/26/136486
                 path.setText(url);
                 web.loadUrl(url);
+                return true;
+            }
+        });
+        web.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,final JsResult result) {
+                AlertDialog Alert=new AlertDialog.Builder(context).setTitle("網頁訊息").setMessage(message).
+                        setPositiveButton("確認", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        }).create();
+                Alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                Alert.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        result.cancel();
+                    }
+                });
+                Alert.show();
+                //return true後絕對不能少了result.confirm()或result.cancel()，不然網頁會卡住
+                //Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+                //return super.onJsAlert(view, url, message, result);
+                return true;
+            }
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message,final JsResult result) {
+                AlertDialog Confirm=new AlertDialog.Builder(context).setTitle("網頁訊息").setMessage(message).
+                        setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.cancel();
+                    }
+                }).create();
+                Confirm.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        result.cancel();
+                    }
+                });
+                Confirm.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                Confirm.show();
+                //return super.onJsConfirm(view,url,message,result);
+                return true;
+            }
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message,String defaultValue, final JsPromptResult result) {
+                final EditText editText=new EditText(context);
+                editText.setText(defaultValue);
+                AlertDialog Prompt=new AlertDialog.Builder(context).setTitle(message).setView(editText).
+                        setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm(editText.getText().toString());
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.cancel();
+                    }
+                }).create();
+                Prompt.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        result.cancel();
+                    }
+                });
+                Prompt.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                Prompt.show();
+                //return super.onJsPrompt(view,url,message,defaultValue,result);
                 return true;
             }
         });
