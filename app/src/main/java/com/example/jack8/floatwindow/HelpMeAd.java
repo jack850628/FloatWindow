257d15921eb423af90be5636d30c4597bfc00106
奇幻 <30308931+jack850628@users.noreply.github.com>
2019-11-12 12:03:26 +0800

    修正獎勵廣告

--- app/src/main/java/com/example/jack8/floatwindow/HelpMeAd.java
+++ app/src/main/java/com/example/jack8/floatwindow/HelpMeAd.java
@@ -19,7 +19,12 @@ public class HelpMeAd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         final RewardedAd rewardedAd = new RewardedAd(this, "ca-app-pub-4604853118314154/2603122586");
-        rewardedAd.loadAd(new AdRequest.Builder().build(), new RewardedAdLoadCallback(){
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("6B58CCD0570D93BA1317A64BEB8BA677")
+                .addTestDevice("1E461A352AC1E22612B2470A43ADADBA")
+                .addTestDevice("F4734F4691C588DB93799277888EA573")
+                .build();
+        rewardedAd.loadAd(adRequest, new RewardedAdLoadCallback(){
             @Override
             public void onRewardedAdLoaded() {
                 rewardedAd.show(HelpMeAd.this, new RewardedAdCallback(){
