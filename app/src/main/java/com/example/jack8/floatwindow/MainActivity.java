a3749bc5dc5972e7618dfc9519853c9a6d0b39e1
jack850628 <jack850628@gmail.com>
2022-01-13 12:23:26 +0800

    更新取得權限的方法

--- app/src/main/java/com/example/jack8/floatwindow/MainActivity.java
+++ app/src/main/java/com/example/jack8/floatwindow/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.jack8.floatwindow;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        startFloatWindow();
+                    }else{
+                        finish();
+                    }
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.window);
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M&&!Settings.canDrawOverlays(MainActivity.this))
-            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + MainActivity.this.getPackageName())), 1);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && !Settings.canDrawOverlays(MainActivity.this))
+            activityResultLauncher.launch(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + MainActivity.this.getPackageName())));
         else {
             startFloatWindow();
         }
@@ -38,15 +56,5 @@ public class MainActivity extends AppCompatActivity {
                 startForegroundService(intent);
         }
         finish();
-    }
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1) {
-            if (Settings.canDrawOverlays(this))
-                startFloatWindow();
-            else
-                finish();
-        }
     }
 }
